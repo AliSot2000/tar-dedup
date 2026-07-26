9a2b029f2c70cd2a04e0cbc425711cecad5af045
AliSot2000 <alisot200@gmail.com>
2026-07-26 17:29:25 +0200

    fix: Special treatment only in case of compression

--- crates/tar-dedup/src/compression.rs
+++ crates/tar-dedup/src/compression.rs
@@ -5,7 +5,7 @@ use crate::config::CompressionFormat;
 pub use xz::InterruptibleXzEncoder;
 
 pub fn warn_on_start(format: CompressionFormat) {
-    if format.allows_resume() {
+    if format.does_compress() {
         eprintln!(
             "warning: each pause finalizes a compression stream; repeated interrupts increase archive size."
         );
