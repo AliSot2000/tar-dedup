7e0a3687decf6b1bafbfa250af5a50b4dc71703f
AliSot2000 <alisot200@gmail.com>
2026-07-29 16:50:34 +0200

    feat: TOD

--- crates/tar-dedup/src/compression.rs
+++ crates/tar-dedup/src/compression.rs
@@ -5,6 +5,7 @@ use crate::config::CompressionFormat;
 pub use xz::InterruptibleXzEncoder;
 
 pub fn warn_on_start(format: CompressionFormat) {
+    // TODO: Needs to be tracing
     if format.does_compress() {
         eprintln!(
             "warning: each pause finalizes a compression stream; repeated interrupts increase archive size."
