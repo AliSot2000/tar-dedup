fb5e111f655babc916b177ffe7fa3193a7fff1a1
AliSot2000 <alisot200@gmail.com>
2026-08-13 11:31:48 +0200

    feat: Util from WalkDir to determine device number

--- crates/tar-dedup/src/common/files.rs
+++ crates/tar-dedup/src/common/files.rs
@@ -131,3 +131,27 @@ fn file_atime(meta: &std::fs::Metadata) -> io::Result<DateTime<Utc>> {
 fn file_ctime(meta: &std::fs::Metadata) -> io::Result<DateTime<Utc>> {
     Ok(DateTime::<Utc>::from(meta.created()?))
 }
+
+// INFO: Section copied from WalkDir::util, (what works - works)
+#[cfg(unix)]
+pub fn device_num<P: AsRef<Path>>(path: P) -> io::Result<u64> {
+    use std::os::unix::fs::MetadataExt;
+
+    path.as_ref().metadata().map(|md| md.dev())
+}
+
+#[cfg(windows)]
+pub fn device_num<P: AsRef<Path>>(path: P) -> io::Result<u64> {
+    use winapi_util::{file, Handle};
+
+    let h = Handle::from_path_any(path)?;
+    file::information(h).map(|info| info.volume_serial_number())
+}
+
+#[cfg(not(any(unix, windows)))]
+pub fn device_num<P: AsRef<Path>>(_: P) -> io::Result<u64> {
+    Err(io::Error::new(
+        io::ErrorKind::Other,
+        "walkdir: same_file_system option not supported on this platform",
+    ))
+}
