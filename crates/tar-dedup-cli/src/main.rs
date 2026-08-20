461aa2e6aa4eec203c009d63f29468b698215059
AliSot2000 <alisot200@gmail.com>
2026-08-21 01:57:32 +0200

    feat: Incomplete resume

--- crates/tar-dedup-cli/src/main.rs
+++ crates/tar-dedup-cli/src/main.rs
@@ -2,6 +2,8 @@ use clap::Parser;
 
 use tar_dedup::cli::{Cli, Command};
 use tar_dedup::config::Config;
+use tar_dedup::db::Database;
+use tar_dedup::error::Error;
 use tar_dedup::shutdown::Shutdown;
 
 fn main() -> tar_dedup::error::Result<()> {
@@ -20,6 +22,20 @@ fn main() -> tar_dedup::error::Result<()> {
         Command::Extract(args) => {
             let config = Config::from_extract_args(&args)?;
             tar_dedup::unarchive::run(config, shutdown)
+        }
+        Command::Resume(args) => {
+            // TODO separate file.
+            let config = Config::from_resume_args(&args)?;
+            let db = Database::open(&config.db_path())?;
+            if db.load_runtime_state()?.is_some() {
+                tar_dedup::archive::run(config, shutdown)
+            } else if db.load_extract_runtime_state()?.is_some() {
+                tar_dedup::unarchive::run(config, shutdown)
+            } else {
+                Err(Error::Config(
+                    "no incomplete archive or extract state in work directory".into(),
+                ))
+            }
         }
     }
 }
