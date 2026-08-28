213ca2bbfc0239648e6ed7f8fa2b8f3cb6dc8416
AliSot2000 <alisot200@gmail.com>
2026-08-29 01:47:57 +0200

    feat: Added place holders for extraction

---
+++ crates/tar-dedup/src/db/permissions.rs
@@ -0,0 +1,2 @@
+// INFO: File is a place holder. All utilities needed by this phase are already covered
+//  by different files
---
+++ crates/tar-dedup/src/db/place.rs
@@ -0,0 +1,5 @@
+use rusqlite::Connection;
+
+use crate::db::common::SqlFileRow;
+use crate::error::Result;
+
