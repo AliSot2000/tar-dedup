d36a90ae075b2f3297d0b1c1546c1aab43df8283
AliSot2000 <alisot200@gmail.com>
2026-09-04 17:03:12 +0200

    feat: Added canonical id

--- crates/tar-dedup/src/db/schema.rs
+++ crates/tar-dedup/src/db/schema.rs
@@ -86,6 +86,7 @@ CREATE INDEX IF NOT EXISTS idx_ref_file ON ref(file_id);
 
 CREATE TABLE IF NOT EXISTS out_tree (
     id       INTEGER PRIMARY KEY CHECK (id > 0),
+    canonical_id INTEGER REFERENCES out_tree(id),
     abs_path TEXT NOT NULL UNIQUE,
     file_id  INTEGER REFERENCES files(id),
     flags    INTEGER NOT NULL DEFAULT 0
