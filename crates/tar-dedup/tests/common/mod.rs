229eddbc1e0ced343848b1422b7930525149f8c8
AliSot2000 <alisot200@gmail.com>
2026-09-13 21:39:44 +0200

    refactor: Adapted

--- crates/tar-dedup/tests/common/mod.rs
+++ crates/tar-dedup/tests/common/mod.rs
@@ -186,6 +186,7 @@ pub fn place_config(extraction_root: PathBuf, absolute_names: bool) -> ExtractCo
             apply_group: false,
             apply_owner: false,
         },
+        mode_policy: tar_dedup::common::perms::ModeSource::None,
     }
 }
 
