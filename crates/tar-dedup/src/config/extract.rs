641a6c43eb1b932054686899adbeefc832b61170
AliSot2000 <alisot200@gmail.com>
2026-09-14 23:30:54 +0200

    refactor: Formatting

--- crates/tar-dedup/src/config/extract.rs
+++ crates/tar-dedup/src/config/extract.rs
@@ -31,9 +31,9 @@ pub struct PlacementOptions {
     pub link_tree: bool,
     pub use_hard_links: bool,
     pub absolute_links: bool, // TODO not possible with use_hard_links
-    pub clean_target: bool, // TODO implies no_create_dir is false!
-    pub link_source: Option<PathBuf>,  // TODO CLI + san!!! (may be file name only), need not exist when starting extraction
-    pub no_reflink: bool, // TODO cli
+    pub clean_target: bool,   // TODO implies no_create_dir is false!
+    pub link_source: Option<PathBuf>, // TODO CLI + san!!! (may be file name only), need not exist when starting extraction
+    pub no_reflink: bool,             // TODO cli
     pub hard_link_grouping: HardLinkGrouping, // TODO cli
     pub recreate_none_file_entries: bool, // TODO CLI
 }
@@ -104,10 +104,8 @@ fn resolve_owner_policy_from_args(
                  explicit --owner/--group/--owner-map/--group-map take precedence"
             );
         }
-        let owner_map = resolve_map_arg(args.owner_map.as_ref(),
-                                        "--owner-map", directory)?;
-        let group_map = resolve_map_arg(args.group_map.as_ref(),
-                                        "--group-map", directory)?;
+        let owner_map = resolve_map_arg(args.owner_map.as_ref(), "--owner-map", directory)?;
+        let group_map = resolve_map_arg(args.group_map.as_ref(), "--group-map", directory)?;
         match parse_owner_group_args(
             args.owner.as_deref(),
             owner_map.as_deref(),
