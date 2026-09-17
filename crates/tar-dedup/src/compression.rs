e43d912aeb3dbe9a5d05d84afc996d01f4238b3f
AliSot2000 <alisot200@gmail.com>
2026-09-17 19:40:47 +0200

    refactor: Switched to tracing 1

--- crates/tar-dedup/src/compression.rs
+++ crates/tar-dedup/src/compression.rs
@@ -7,10 +7,10 @@ pub use xz::{
 };
 
 pub fn warn_on_start(format: CompressionFormat) {
-    // TODO: Needs to be tracing
     if format.does_compress() {
-        eprintln!(
-            "warning: each pause finalizes a compression stream; repeated interrupts increase archive size."
+        tracing::warn!(
+            "warning: each pause finalizes a compression stream; repeated interrupts increase \
+            archive size."
         );
     }
 }
